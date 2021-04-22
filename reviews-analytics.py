949385e95cb6bd43b8837d9414cbe729601c4a38
Allen Wu <wing650503@gmail.com>
2021-04-23 00:08:48 +0800

    import module time and progressbar

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -1,12 +1,18 @@
-data=[]
-count=0
+import time
+import progressbar #pip install progressbar2 pip套件管理員
+ 
+data = []
+count = 0
+bar = progressbar.ProgressBar(max_value = 1000000) #ProgressBar是物件，有自己的型別 是因為用class寫成的。物件第一個字一定大寫
 with open ('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
 		count+=1
-		if count % 1000 ==0:
-			print(len(data))
-print('檔案讀取完了，總共有', len(data), '筆資料')
+		bar.update(count) #bar這個物件有update功能，因為在寫class裡面有update這個功能
+		# if count % 1000 ==0:
+		# 	print(len(data)) 顯示每1000筆的進度用套件取代
+
+print('檔案讀取完了，總共有', len(data), '筆資料') #print是function，function第一個英文字一定小寫
 sum_len = 0
 for d in data:
 	sum_len += len(d)
@@ -34,6 +40,7 @@ print('一共有', len(good), '筆留言提到good')
 # print(bad)
 
 #文字計數
+start_time = time.time()
 wc = {} #word_count
 for d in data:
 	words = d.split() #如果有連續3個空字串 ''會切成3分 但若直接打split()會跳過不切
@@ -45,6 +52,10 @@ for d in data:
 for word in wc:
 	if wc[word] > 1000000: 
 		print(word, wc[word])
+end_time = time.time()
+print('花了', end_time - start_time, 'seconds')
+print(len(wc)) #字典有幾個key
+print(wc['Allen']) #字典中Allen出現幾次(value)
 
 while True:
 	word = input('請問你想查什麼字: ')
